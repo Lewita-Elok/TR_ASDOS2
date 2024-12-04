594b1b1b95db2e595fea7c3e290f53d0b55c67c7
dheardna <ardinaangel1@gmail.com>
2024-12-04 16:35:44 +0700

    cust new

--- dashboard/customer_dashboard.php
+++ dashboard/customer_dashboard.php
@@ -9,9 +9,16 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'customer') {
 }
 
 // Query untuk melihat stok barang
-$stok_barang_query = "SELECT * FROM barang WHERE status='Masuk' OR status='Keluar'";
-$stok_barang_result = mysqli_query($conn, $stok_barang_query);
+$stok_masuk_query = "SELECT * FROM barang";
+$stok_masuk_result = mysqli_query($conn, $stok_masuk_query);
+
+$stok_keluar_query = "SELECT struk.id_struk, struk.id_barang, struk.jumlah, struk.status, barang.id_barang, barang.nama_barang
+FROM struk
+Join barang ON struk.id_barang = barang.id_barang";
+$stok_keluar_result = mysqli_query($conn, $stok_keluar_query);
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="id">
@@ -66,11 +73,19 @@ $stok_barang_result = mysqli_query($conn, $stok_barang_query);
                 </thead>
                 <tbody>
                     <?php
-                    while ($row = mysqli_fetch_assoc($stok_barang_result)) {
+                    while ($row = mysqli_fetch_assoc($stok_masuk_result)) {
                         echo "<tr class='hover:bg-gray-200'>
                                 <td class='border border-gray-300 px-4 py-2'>{$row['id_barang']}</td>
                                 <td class='border border-gray-300 px-4 py-2'>{$row['nama_barang']}</td>
                                 <td class='border border-gray-300 px-4 py-2'>{$row['stok']}</td>
+                                <td class='border border-gray-300 px-4 py-2'>{$row['status']}</td>
+                            </tr>";
+                    }
+                    while ($row = mysqli_fetch_assoc($stok_keluar_result)) {
+                        echo "<tr class='hover:bg-gray-200'>
+                                <td class='border border-gray-300 px-4 py-2'>{$row['id_struk']}</td>
+                                <td class='border border-gray-300 px-4 py-2'>{$row['nama_barang']}</td>
+                                <td class='border border-gray-300 px-4 py-2'>{$row['jumlah']}</td>
                                 <td class='border border-gray-300 px-4 py-2'>{$row['status']}</td>
                             </tr>";
                     }
